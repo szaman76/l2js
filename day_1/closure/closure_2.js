6cb16bfb17efe78163784df6a3f35d15dce7044e
Michal Szymanski <m.szymanski1995@gmail.com>
2017-12-03 00:41:52 +0100

    iife closure iteration
    using iife and closure to iterate for loop

--- day_1/closure/closure_2.js
+++ day_1/closure/closure_2.js
@@ -10,3 +10,9 @@ for(var i = 1; i<=5; i++) {
 // must use IIFE to make more scope
 // then create new var "j" and use it as a copy of "i"
 // can place j as parameter in anonymous function like function(j)
+/*
+The use of an IIFE inside each iteration created a new scope for each iteration, which gave our timeout function
+callbacks the opportunity to close over a new scope for each iteration, one which had a variable with the right 
+per-iteration value in it for us to acces.
+
+*/
