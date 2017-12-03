9885b5a6127cacfef59274ee40d71c16417a9c81
Michal Szymanski <m.szymanski1995@gmail.com>
2017-12-03 18:10:45 +0100

    think about it

---
+++ day_1/module/foo_bar/foo.js
@@ -0,0 +1,11 @@
+// import only "hello() from the "bar" module"
+import hello from "bar";
+
+var hungry = "hippo";
+
+function awesome() {
+    console.log(
+        hello(hungry).toUpperCase()
+    );
+}
+export awesome;
