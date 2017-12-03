064096f33026783dc5b7ec2155b5bb3e9ed121e7
Michal Szymanski <m.szymanski1995@gmail.com>
2017-12-03 19:18:02 +0100

    appendix

---
+++ day_1/scope/appendix_2.js
@@ -0,0 +1,19 @@
+// Lexical-this
+
+{             // arrow function declaration
+    var foo = a => {
+        console.log(a);
+    };
+    foo(2); // 2
+}
+{
+    var obj = {
+        id: "awesome",
+        cool: function coolFn() {
+                console.log(this.id);
+        }
+    };
+    var id = "not awesome";
+    obj.cool(); // awesome
+    setTimeout(obj.cool,1000); //undefined even tho it should or not be "not awesome"
+}
