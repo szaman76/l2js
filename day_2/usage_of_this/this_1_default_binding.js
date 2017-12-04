6c1c3ec0ad353b99251542cf7ac9df7233f12a33
Michal Szymanski <m.szymanski1995@gmail.com>
2017-12-04 21:46:39 +0100

    call-site

--- day_2/usage_of_this/this_1_default_binding.js
+++ day_2/usage_of_this/this_1_default_binding.js
@@ -1,5 +1,6 @@
 //default catch-all
-
+//1. var a = 2; is synonymous with global-object properties of the same name
+//2. when foo() is called, this.a resolves to our global variable "a"
 function foo() {
 
     console.log(this.a);
