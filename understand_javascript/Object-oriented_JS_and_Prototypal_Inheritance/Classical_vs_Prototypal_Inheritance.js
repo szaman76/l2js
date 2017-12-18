8be9047dd626f1cf10972413af9a1158416c6aba
Michal Szymanski <m.szymanski1995@gmail.com>
2017-12-18 19:40:44 +0100

    różnice

---
+++ understand_javascript/Object-oriented_JS_and_Prototypal_Inheritance/Classical_vs_Prototypal_Inheritance.js
@@ -0,0 +1,20 @@
+// Inheritance: 
+//              One object gets access to the properties and methods of another object.
+
+
+// Classical Inheritance:
+//      Verbose.(rozwlekły)
+/*
+            friend
+            protected
+            private
+            interface
+*/
+
+// Prototypal Inheritance:
+//      Simple
+/*
+            flexible
+            extensible(elastyczny)
+            easy to understand
+*/
