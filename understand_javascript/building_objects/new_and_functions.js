548be9fb9ae6f911b47fa18bacf5fc2a089d3bf3
Michal Szymanski <m.szymanski1995@gmail.com>
2017-12-19 00:02:51 +0100

    remember remember

---
+++ understand_javascript/building_objects/new_and_functions.js
@@ -0,0 +1,6 @@
+// DANGER "new" and functions
+
+// Always remember to use "new" before function name or it returns undefined
+
+// Any function that we intend to be a function constructor we use a capital letter for its name.
+
