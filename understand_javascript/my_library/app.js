994eab3ba7d02f8c438e8014710ba7c38a767051
Michal Szymanski <m.szymanski1995@gmail.com>
2017-12-19 22:47:55 +0100

    end

--- understand_javascript/my_library/app.js
+++ understand_javascript/my_library/app.js
@@ -1,4 +1,22 @@
+
+//gets a new object (the architecture allows us to not have to use the "new" keyword here)
 var g = G$("Michal", "Szymanski"); 
+
 console.log(g); // Michal Szymanski en
+
+//use our chainable methods
 g.greet().setLang("pl").greet(true); // Hello Michal! // Witaj Michal Szymanski.
 g.greet().setLang("es").greet(true); // Hello Michal! // Uncaught Invalid language
+
+//lets use our object on the click of the login button
+$("#login").click(function(){
+    
+    //create a new "Greetr" object (lets pretend we know the name from the login)
+    var loginGrtr = G$("Michal", "Szymanski");
+
+    //hide the login on the screen
+    $("#logindiv").hide();
+
+    //fire off an HTML greeting, passing the "#greeting" as the selector and the chosen language, and log the welcome as well
+    loginGrtr.setLang($("#lang").val()).HTMLGreeting("#greeting",true).log();
+});
