447f0fc350b7b92cd48515d9b9cfc707a4aa05a5
Michal Szymanski <m.szymanski1995@gmail.com>
2017-12-19 22:07:30 +0100

    app logs

--- understand_javascript/my_library/app.js
+++ understand_javascript/my_library/app.js
@@ -1,2 +1,4 @@
 var g = G$("Michal", "Szymanski"); 
 console.log(g); // Michal Szymanski en
+g.greet().setLang("pl").greet(true); // Hello Michal! // Witaj Michal Szymanski.
+g.greet().setLang("es").greet(true); // Hello Michal! // Uncaught Invalid language
