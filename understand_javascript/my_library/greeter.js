d74a1ea93ea60268bd834f0af92f7bfb3dfb4c97
Michal Szymanski <m.szymanski1995@gmail.com>
2017-12-19 20:42:45 +0100

    structuring safe code

---
+++ understand_javascript/my_library/greeter.js
@@ -0,0 +1,14 @@
+/*
+GREETR
+
+When given a first name, last name, and optional language, it generates formal and informal greetings.
+
+Suport English and Polish.
+
+Reusable library/framework
+
+Easy to type "G$()" structure.
+
+Support jQuery
+*/
+
